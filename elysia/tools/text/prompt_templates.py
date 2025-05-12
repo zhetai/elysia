f7fd943520d7662ed23d6d8e1afcde1e82919061
dannyjameswilliams <danny@weaviate.io>
2025-05-12 09:53:06 +0100

    added cited summarize tool

--- elysia/tools/text/prompt_templates.py
+++ elysia/tools/text/prompt_templates.py
@@ -1,4 +1,33 @@
 import dspy
+from typing import List
+from elysia.tools.text.objects import TextWithCitation
+
+
+class CitedSummarizingPrompt(dspy.Signature):
+    """
+    Given a user_prompt, as well as a list of retrieved objects from the environment, summarize the information in the objects to answer the user's prompt.
+    You will do so by providing a full summary, via a collection of TextWithCitation objects.
+    The combination of these 'text' fields in these objects will form the full summary (combined via string concatenation).
+    """
+
+    subtitle = dspy.OutputField(description="A subtitle for the summary")
+    cited_text: List[TextWithCitation] = dspy.OutputField(
+        description="""
+        The list of TextWithCitation objects, which will form the full summary when the 'text' fields are concatenated.
+        Use markdown formatting in the 'text' fields to break down the information into sections, bullet points, etc to make it easier to read.
+        Your summary should take account what the user prompt is, and the information in the environment.
+        Your summary should be parsing the retrieved objects, and summarising the information in them that is relevant to the user's prompt.
+        For example, picking the most relevant information from the retrieved objects, and discussing it, repeating ONLY relevant information if necessary.
+        You should provide useful analysis.
+
+        Make sure that your citations are accurate.
+        You can provide cited_text fields which MUST come from the environment.
+        Do not include any citations which are not in the environment.
+
+        Take the _REF_ID from items in the environment, only use them as the ref_ids in the TextWithCitation objects.
+        Do not use the _REF_ID anywhere in the 'text' fields, they should only be copied as the ref_id in the TextWithCitation objects.
+        """.strip()
+    )
 
 
 class SummarizingPrompt(dspy.Signature):
