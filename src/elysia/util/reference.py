f4337b637054ba02a7375831de9ee425f8c4dce8
dannyjameswilliams <danny@weaviate.io>
2025-05-15 15:37:04 +0100

    Deleted unnecessary files and directories

--- src/elysia/util/reference.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def create_reference(date: datetime.datetime = datetime.datetime.now()) -> str:
-    return {
-        "datetime": date.isoformat(),
-        "day_of_week": date.strftime("%A"),
-        "time_of_day": date.strftime("%I:%M %p"),
-    }
